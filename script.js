ce85857244f6c4c6c18408e3ab5220a6a2c2e6d6
intrigasam <samiraidigovav@gmail.com>
2024-05-26 11:04:46 +0300

    сделала переключение отображения корзины

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // ЯВАСКРИПТ
+
+let cartIcon=document.querySelector(".cartIcon")
+let cartModal=document.querySelector(".cartModal")
+
+cartIcon.addEventListener("click",function(){
+  cartModal.classList.toggle("hide")
+
+})
