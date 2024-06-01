0920c23539c1fc8fdd333f36fa13528ef759a660
EvaMushadzhieva <eva_jabrailova@mail.ru>
2024-06-01 12:45:20 +0300

    Изменила корзину

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// ЯВАСКРИПТ
-
-let cartIcon = document.querySelector(".cartIcon");
-let cartModal = document.querySelector(".cartModal");
-
-cartIcon.addEventListener("click", function(){
-    cartModal.classList.toggle("hide")
-})
-
-
 let data = [
     {
         id: 1,
