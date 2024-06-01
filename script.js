a930d25d324922be5a2090f7798c05a310981b49
Yanarkaev <hasssanbek@gmail.com>
2024-06-01 09:48:07 +0300

    добавил картинки

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // ЯВАСКРИПТ
+
+let cartIcon = document.querySelector(".cartIcon");
+let cartModal = document.querySelector(".cartModal");
+
+cartIcon.addEventListener("click", function(){
+    cartModal.classList.toggle("hide")
+})
