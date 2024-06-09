116ccd7e2a8609e6573e1019f2cc44dea5a507e9
intrigasam <samiraidigovav@gmail.com>
2024-06-09 11:00:13 +0300

    сделала цифры на корззине

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 let data = [
-    {
-        id: 1,
-        name: "Ночь у берега",
-        price: 2500000,
-        imgPath: "./assets/photo_2024-06-01_09-45-15.jpg",
-    },
+  {
+    id: 1,
+    name: "Ночь у берега",
+    price: 2500000,
+    imgPath: "./assets/photo_2024-06-01_09-45-15.jpg",
+  },
 
-    {
-        id: 2,
-        name: "Горы за лесами",
-        price: 2800000,
-        imgPath: "./assets/photo_2024-06-01_09-45-22.jpg",
-    },
+  {
+    id: 2,
+    name: "Горы за лесами",
+    price: 2800000,
+    imgPath: "./assets/photo_2024-06-01_09-45-22.jpg",
+  },
 
-    {
-        id: 3,
-        name: "Ассорти",
-        price: 2700000,
-        imgPath: "./assets/photo_2024-06-01_09-45-29.jpg",
-    },
+  {
+    id: 3,
+    name: "Ассорти",
+    price: 2700000,
+    imgPath: "./assets/photo_2024-06-01_09-45-29.jpg",
+  },
 
-    {
-        id: 4,
-        name: "Лето",
-        price: 2200000,
-        imgPath: "./assets/photo_2024-06-01_09-45-43.jpg",
-    },
+  {
+    id: 4,
+    name: "Лето",
+    price: 2200000,
+    imgPath: "./assets/photo_2024-06-01_09-45-43.jpg",
+  },
 
-    {
-        id: 6,
-        name: "Пейзаж гор",
-        price: 2900000,
-        imgPath: "./assets/photo_2024-06-01_09-47-18.jpg",
-    },
+  {
+    id: 6,
+    name: "Пейзаж гор",
+    price: 2900000,
+    imgPath: "./assets/photo_2024-06-01_09-47-18.jpg",
+  },
 ];
 
 let cartData = [];
@@ -41,21 +41,22 @@ let cartIcon = document.querySelector(".cartIcon");
 let cartModal = document.querySelector(".cartModal");
 
 cartIcon.addEventListener("click", function () {
-    cartModal.classList.toggle("hide");
+  cartModal.classList.toggle("hide");
 });
 
 let cartModalLeftBlock = document.querySelector(".cartModalLeftBlock");
 let productCardsWrapper = document.querySelector(".productCardsWrapper");
+let cartIconCount = document.querySelector(".cartIconCount");
 
 document.querySelector(".introbtn").addEventListener("click", function () {
-    productCardsWrapper.scrollIntoView({
-        behavior: "smooth",
-    });
+  productCardsWrapper.scrollIntoView({
+    behavior: "smooth",
+  });
 });
 
 function renderProducts() {
-    for (let i = 0; i < data.length; i++) {
-        productCardsWrapper.innerHTML += `
+  for (let i = 0; i < data.length; i++) {
+    productCardsWrapper.innerHTML += `
         <div class="productCard">
         <div class="productImg">
             <img src=${data[i].imgPath} />
@@ -68,27 +69,34 @@ function renderProducts() {
         </div>
         </div>
         `;
-    }
+  }
 
-    let buyBtns = document.querySelectorAll(".buyBtn");
+  let buyBtns = document.querySelectorAll(".buyBtn");
 
-    for (let i = 0; i < buyBtns.length; i++) {
-        buyBtns[i].addEventListener("click", function () {
-            buyBtns[i].innerHTML = "В корзине";
-            buyBtns[i].disabled = true
-            cartData.push({ ...data[i], count: 1 });
-            renderCart();
-        });
-    }
+  for (let i = 0; i < buyBtns.length; i++) {
+    buyBtns[i].addEventListener("click", function () {
+      buyBtns[i].innerHTML = "В корзине";
+      buyBtns[i].disabled = true;
+      cartData.push({ ...data[i], count: 1 });
+      renderCart();
+    });
+  }
 }
 
 renderProducts();
 
 function renderCart() {
-    console.log(cartData)
-    cartModalLeftBlock.innerHTML = "";
-    for (let i = 0; i < cartData.length; i++) {
-        cartModalLeftBlock.innerHTML += `
+  if (cartData.length === 0) {
+    cartIconCount.style.display = "none";
+  } else {
+    cartIconCount.style.display = "flex";
+  }
+
+  cartIconCount.innerHTML = cartData.length;
+
+  cartModalLeftBlock.innerHTML = "";
+  for (let i = 0; i < cartData.length; i++) {
+    cartModalLeftBlock.innerHTML += `
     <div class="cartModalProduct">
     <div class="cartModalProductImg">
     <img src=${cartData[i].imgPath} />
@@ -103,22 +111,23 @@ function renderCart() {
       <button class="cartModalCounterInc">+</button>
     </div>
   </div>`;
+  }
 
-}
+  let incBtns = document.querySelectorAll(".cartModalCounterInc");
+  let decBtns = document.querySelectorAll(".cartModalCounterDec");
+  let productCount = document.querySelectorAll(".cartModalProductCount");
 
-let incBtns = document.querySelectorAll(".cartModalCounterInc");
-let decBtns = document.querySelectorAll(".cartModalCounterDec");
-let productCount = document.querySelectorAll(".cartModalProductCount");
-
-for (let i = 0; i < cartData.length; i++) {
+  for (let i = 0; i < cartData.length; i++) {
     incBtns[i].addEventListener("click", function () {
-        cartData[i].count += 1;
-        productCount[i].innerHTML = cartData[i].count;
+      cartData[i].count += 1;
+      productCount[i].innerHTML = cartData[i].count;
     });
 
     decBtns[i].addEventListener("click", function () {
-        cartData[i].count -= 1;
-        productCount[i].innerHTML = cartData[i].count;
+      cartData[i].count -= 1;
+      productCount[i].innerHTML = cartData[i].count;
     });
+  }
 }
-}
+
+renderCart();
